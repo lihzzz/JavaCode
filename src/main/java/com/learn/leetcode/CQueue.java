0661299b7efd75227b3a2527f3321340e7f11ff6
朱淑媛 <34476629+zhushuyuan0601@users.noreply.github.com>
2022-12-03 21:37:58 +0800

    代码结构调整

---
+++ src/main/java/com/learn/leetcode/CQueue.java
@@ -0,0 +1,39 @@
+package com.learn.leetcode;
+
+import java.util.Stack;
+
+/**
+ * @author lihzz
+ * @version 1.0
+ * @date 2022/12/1 23:45
+ */
+public class CQueue {
+    private Stack<Integer> stack1;
+    private Stack<Integer> stack2;
+
+    private boolean flag;
+
+    public CQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+        flag = true;
+    }
+
+    public void appendTail(int value) {
+        stack1.push(value);
+    }
+
+    public int deleteHead() {
+        if(stack1.isEmpty()){
+            return -1;
+        }
+        while(!stack1.isEmpty()){
+            stack2.push(stack1.pop());
+        }
+        int result = stack2.pop();
+        while (!stack2.isEmpty()){
+            stack1.push(stack2.pop());
+        }
+        return result;
+    }
+}
